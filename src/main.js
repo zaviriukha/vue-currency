1e59327d38a0a26137194f2c4d0b4f293504b86d
Val <v.zaviriukha@gmail.com>
2020-10-16 15:23:30 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+import { ValidationProvider } from 'vee-validate';
 
 Vue.config.productionTip = false
+
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
